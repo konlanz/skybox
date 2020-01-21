20b801f40a9bcbcf986f5709d8d3482d79620cd1
zionkonlan <konlanmikpekoah.km@outlook.com>
2020-01-21 00:44:15 +0000

    jk

--- config/config.js
+++ config/config.js
@@ -2,8 +2,9 @@
 var mongodb = require('mongodb');
 var mongoose = require('mongoose');
  var dburl = "mongodb://localhost:27017/skybox";
+ var dburi = "mongodb+srv://mkonlan:5hinvi5@zion@cluster0-uylml.mongodb.net/test?retryWrites=true&w=majority"
 mongoose.Promise = global.Promise;
- mongoose.connect(dburl, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }).then(
+ mongoose.connect(dburi, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }).then(
    () => {console.log('Database is connected') },
    err => { console.log('Can not connect to the database'+ err)}
  );
